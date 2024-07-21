2347de2873296635e89470484e4c74a1526bd014
Gustavo Josue Robles Martinez <roblesmgustavoj@gmail.com>
2024-07-21 21:57:09 +0000

    removed nginx ingress test

--- 01-nginx.tf
+++ 01-nginx.tf
@@ -10,19 +10,3 @@ resource "kubectl_manifest" "nginx" {
     for_each  = data.kubectl_file_documents.nginx.manifests
     yaml_body = each.value
 }
-
-data "http" "nginx_test" {
-  url = "https://kind.sigs.k8s.io/examples/ingress/usage.yaml"
-}
-
-data "kubectl_file_documents" "nginx_test" {
-    content = data.http.nginx_test.response_body
-}
-
-resource "kubectl_manifest" "nginx_test" {
-    depends_on = [
-      kubectl_manifest.nginx
-    ]
-    for_each  = data.kubectl_file_documents.nginx_test.manifests
-    yaml_body = each.value
-}
